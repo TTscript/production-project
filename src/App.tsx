fa6ad6ca977ce80038f09725d2d4e3b4efeb0347
Timur Tokumov <t.tokumov@gmail.com>
2024-09-21 17:36:53 +0500

    feat: add react router dom, add routing to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import Counter from "./components/Counter";
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./components/pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./components/pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            sadflkjasoldfjsf
-            <Counter />
+            <Link to={'/'}>MAIN PAGE</Link>
+            <Link to={'/about'}>ABOUT PAGE</Link>
+            <Suspense fallback={'Loading the page...'}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
+
         </div>
     );
 };
